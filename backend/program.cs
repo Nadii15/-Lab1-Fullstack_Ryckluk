7c932018c32d4511e98c12e35f3896b8feea5043
okolodk <grisha.zhivot@bk.ru>
2026-01-15 19:08:54 +0300

    name

--- backend/program.cs
+++ backend/program.cs
@@ -2,3 +2,7 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Меня зовут Рыхлюк Н.В.");
 Console.WriteLine(DateTime.Now);
+
+Console.Write("Введите имя: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Привет,{name}");
